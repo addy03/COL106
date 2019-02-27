c8d7b13c14c6d8bb1aca9e590f623841fadb9d6c
arneish <arneish.p@gmail.com>
2019-02-27 16:31:44 +0530

    edited

--- Assgn2_re/src/home.java
+++ Assgn2_re/src/home.java
@@ -3,47 +3,41 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class home
-{
-    public static void main(String args[])
-    {
-//        String workingDir = System.getProperty("user.dir");
-//        System.out.println("Current working directory : " + workingDir);
+public class home {
+    public static void main(String args[]) {
+        // String workingDir = System.getProperty("user.dir");
+        // System.out.println("Current working directory : " + workingDir);
         employee emp = new employee("CEO");
-        Scanner input = new Scanner(new File ("input_small.txt"));
+        try {
+            Scanner input = new Scanner(new File(args[0]));
 
-        String answer = input.nextLine();
-        int val = Integer.parseInt(answer);
-        for (int i=0; i<val; i++)
-        {
+            String answer = input.nextLine();
+            int val = Integer.parseInt(answer);
+            for (int i = 0; i < val; i++) {
+                answer = input.nextLine();
+                String[] splited = answer.split(" ");
+                emp.AddEmployee(splited[0], splited[1]);
+            }
+
             answer = input.nextLine();
-            String[] splited = answer.split(" ");
-            emp.AddEmployee(splited[0], splited[1]);
-        }
+            int op = Integer.parseInt(answer);
 
-        answer = input.nextLine();
-        int op = Integer.parseInt(answer);
-
-        for (int i=0; i<op; i++)
-        {
-            String[] splited = answer.split(" ");
-            int a = Integer.parseInt(splited[0]);
-            if (a == 0)
-            {
-                emp.AddEmployee(splited[1], splited[2]);
+            for (int i = 0; i < op; i++) {
+                String[] splited = answer.split(" ");
+                int a = Integer.parseInt(splited[0]);
+                if (a == 0) {
+                    emp.AddEmployee(splited[1], splited[2]);
+                } else if (a == 1) {
+                    emp.DeleteEmployee(splited[1], splited[2]);
+                } else if (a == 2) {
+                    emp.LowestCommonBoss(splited[1], splited[2]);
+                } else if (a == 3) {
+                    emp.PrintEmployees();
+                }
             }
-            else if(a == 1)
-            {
-                emp.DeleteEmployee(splited[1], splited[2]);
-            }
-            else if(a == 2)
-            {
-                emp.LowestCommonBoss(splited[1], splited[2]);
-            }
-            else if(a == 3)
-            {
-                emp.PrintEmployees();
-            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("failed");
+            // insert code to run when exception occurs
         }
 
     }
