3796496fca384e25d974812ea6cde0e0634caca0
Aditya Gupta <adityag1999@gmail.com>
2019-02-27 16:11:40 +0530

    home

--- Assgn2_re/src/home.java
+++ Assgn2_re/src/home.java
@@ -1,17 +1,50 @@
+import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class home
 {
     public static void main(String args[])
     {
-        employee df = new employee("Aditya");
-        df.AddEmployee("abc", "Aditya");
-        df.AddEmployee("abc2", "Aditya");
-        df.AddEmployee("abc3", "Aditya");
-        df.AddEmployee("abc_1", "abc");
-        df.AddEmployee("abc_2", "abc");
-        df.AddEmployee("Aditya", "abc2");
-        df.DeleteEmployee("abc","abc");
-        df.LowestCommonBoss("abc","abc_1");
-        df.PrintEmployees();
+//        String workingDir = System.getProperty("user.dir");
+//        System.out.println("Current working directory : " + workingDir);
+        employee emp = new employee("CEO");
+        Scanner input = new Scanner(new File ("input_small.txt"));
+
+        String answer = input.nextLine();
+        int val = Integer.parseInt(answer);
+        for (int i=0; i<val; i++)
+        {
+            answer = input.nextLine();
+            String[] splited = answer.split(" ");
+            emp.AddEmployee(splited[0], splited[1]);
+        }
+
+        answer = input.nextLine();
+        int op = Integer.parseInt(answer);
+
+        for (int i=0; i<op; i++)
+        {
+            String[] splited = answer.split(" ");
+            int a = Integer.parseInt(splited[0]);
+            if (a == 0)
+            {
+                emp.AddEmployee(splited[1], splited[2]);
+            }
+            else if(a == 1)
+            {
+                emp.DeleteEmployee(splited[1], splited[2]);
+            }
+            else if(a == 2)
+            {
+                emp.LowestCommonBoss(splited[1], splited[2]);
+            }
+            else if(a == 3)
+            {
+                emp.PrintEmployees();
+            }
+        }
 
     }
 }
